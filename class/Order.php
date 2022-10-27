3bdb47da185af92d9596a05886e5a51232d1440d
faustddd <authur82@mail.ru>
2022-10-27 19:13:32 +0500

    end

--- class/Order.php
+++ class/Order.php
@@ -15,6 +15,54 @@ class Order
 
     }
 
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param string $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = (string)$number;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSum()
+    {
+        return $this->sum;
+    }
+
+    /**
+     * @param float $sum
+     */
+    public function setSum($sum)
+    {
+        $this->sum = (string)$sum;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $action
+     */
+    public function setAction($action)
+    {
+        $this->action = (string)$action;
+    }
+
 
 
 
--- class/Orders.php
+++ class/Orders.php
@@ -1,16 +1,99 @@
 <?php
 require_once('Order.php');
+require_once('Config.php');
+
 class Orders
 {
-    protected $config;
+    private $config;
     private $orders = [];
 
-    public function __construct($config)
+    public function __construct()
     {
-        if ($config && !empty($config)){
-            $this->config = $config;
+        $this->config = new Config();
+        $this->getOrdersFromXml();
+        $this->saveOrdersInBD();
+    }
+
+    /**
+     * @return array
+     */
+    public function getOrders()
+    {
+        return $this->orders;
+    }
+
+    /**
+     * @param array $orders
+     */
+    public function addOrder($order)
+    {
+        $this->orders[] = $order;
+    }
+
+    public function getOrdersFromXml()
+    {
+        $dir_orders = $this->config->getPathImportOrdersDir();
+        $open_dir = opendir($dir_orders);
+        while (false !== ($file = readdir($open_dir))) {
+            if ($file != '.' && $file != '..' && !is_dir($dir_orders . '/' . $file) && $file[0] != '.') {
+                $orders_xml = simplexml_load_file($dir_orders . '/' . $file);
+
+                foreach ($orders_xml->ordersPayments->order as $order_xml) {
+                    $Order = new Order();
+                    $Order->setNumber($order_xml['Number']);
+                    $Order->setSum($order_xml['Sum']);
+                    $Order->setAction('payments');
+                    $this->addOrder($Order);
+                }
+                foreach ($orders_xml->ordersNew->order as $order_xml) {
+
+                    $Order = new Order();
+                    $Order->setNumber($order_xml['Number']);
+                    $Order->setSum($order_xml['Sum']);
+                    $Order->setAction('new');
+
+                    $this->addOrder($Order);
+                }
+            }
         }
     }
 
+    public function saveOrdersInBD()
+    {
+        $login_bd = $this->config->getLoginDb();
+        $password_bd = $this->config->getPasswordDb();
+        $name_bd = $this->config->getNameBd();
+        $conn = new mysqli("localhost", $login_bd, $password_bd, $name_bd);
 
+        if ($conn->connect_error) {
+            die("Ошибка: " . $conn->connect_error);
+        }
+
+        if (!empty($this->orders)) {
+            foreach ($this->getOrders() as $order) {
+
+                $order_name = $order->getNumber();
+                $order_sum = $order->getSum();
+                $order_name = '56112';
+                $sql_test = "SELECT *  FROM auvf_yandexclient WHERE `name` = '$order_name'";
+                $row = $conn->query($sql_test)->num_rows;
+                $sql = '';
+                if ($order->getAction() === 'payments' && $row >= 1) {
+                    $sql = "DELETE FROM auvf_yandexclient WHERE `name`= '$order_name'";
+
+                } elseif ($order->getAction() === 'new' && $row == 0) {
+                    $sql = "INSERT INTO `auvf_yandexclient`(`name`, `price`, `status`) VALUES ($order_name, $order_sum, '0')";
+                }
+
+                if (!empty($sql) && $conn->query($sql)) {
+                    echo "Данные успешно добавлены";
+                } else {
+                    echo "Ошибка: " . $conn->error;
+                }
+
+            }
+        }
+
+        $conn->close();
+    }
 }
