acdc75e28f83d5be6c45527146d2725d917be93a
faustddd <authur82@mail.ru>
2022-10-27 14:38:35 +0500

    create class Order

---
+++ class/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+class Order
+{
+    //номер заказа
+    private $number = '';
+    //оплаченная сумма
+    private $sum = 0.0;
+    //new - новый; payments - оплаченный
+    private $action = 'new';
+
+
+    public function __construct()
+    {
+
+    }
+
+
+
+
+}
